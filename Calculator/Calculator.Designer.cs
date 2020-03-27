e8988b2ad76d05162d44f57de613b3d2dd8081da
User2 <User2@pellecarta.onmicrosoft.com>
2020-03-27 17:25:58 +0530

    Add project files.

---
+++ Calculator/Calculator.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Input_Text = new System.Windows.Forms.TextBox();
+            this.FuncMul = new System.Windows.Forms.Button();
+            this.FuncSub = new System.Windows.Forms.Button();
+            this.FuncAdd = new System.Windows.Forms.Button();
+            this.FuncDiv = new System.Windows.Forms.Button();
+            this.FuncCalc = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Num1
+            // 
+            this.Num1.Location = new System.Drawing.Point(12, 60);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(40, 40);
+            this.Num1.TabIndex = 0;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            this.Num1.Click += new System.EventHandler(this.Num1_Click);
+            // 
+            // Num2
+            // 
+            this.Num2.Location = new System.Drawing.Point(86, 60);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(40, 40);
+            this.Num2.TabIndex = 2;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            this.Num2.Click += new System.EventHandler(this.Num2_Click);
+            // 
+            // Num3
+            // 
+            this.Num3.Location = new System.Drawing.Point(160, 60);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(40, 40);
+            this.Num3.TabIndex = 3;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            this.Num3.Click += new System.EventHandler(this.Num3_Click);
+            // 
+            // Num6
+            // 
+            this.Num6.Location = new System.Drawing.Point(160, 124);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(40, 40);
+            this.Num6.TabIndex = 6;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            this.Num6.Click += new System.EventHandler(this.Num6_Click);
+            // 
+            // Num5
+            // 
+            this.Num5.Location = new System.Drawing.Point(86, 124);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(40, 40);
+            this.Num5.TabIndex = 5;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            this.Num5.Click += new System.EventHandler(this.Num5_Click);
+            // 
+            // Num4
+            // 
+            this.Num4.Location = new System.Drawing.Point(12, 124);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(40, 40);
+            this.Num4.TabIndex = 4;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            this.Num4.Click += new System.EventHandler(this.Num4_Click);
+            // 
+            // Num9
+            // 
+            this.Num9.Location = new System.Drawing.Point(160, 188);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(40, 40);
+            this.Num9.TabIndex = 9;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            this.Num9.Click += new System.EventHandler(this.Num9_Click);
+            // 
+            // Num8
+            // 
+            this.Num8.Location = new System.Drawing.Point(86, 188);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(40, 40);
+            this.Num8.TabIndex = 8;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            this.Num8.Click += new System.EventHandler(this.Num8_Click);
+            // 
+            // Num7
+            // 
+            this.Num7.Location = new System.Drawing.Point(12, 188);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(40, 40);
+            this.Num7.TabIndex = 7;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            this.Num7.Click += new System.EventHandler(this.Num7_Click);
+            // 
+            // Input_Text
+            // 
+            this.Input_Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input_Text.Location = new System.Drawing.Point(12, 12);
+            this.Input_Text.Name = "Input_Text";
+            this.Input_Text.Size = new System.Drawing.Size(262, 27);
+            this.Input_Text.TabIndex = 10;
+            // 
+            // FuncMul
+            // 
+            this.FuncMul.Location = new System.Drawing.Point(234, 188);
+            this.FuncMul.Name = "FuncMul";
+            this.FuncMul.Size = new System.Drawing.Size(40, 40);
+            this.FuncMul.TabIndex = 13;
+            this.FuncMul.Text = "*";
+            this.FuncMul.UseVisualStyleBackColor = true;
+            this.FuncMul.Click += new System.EventHandler(this.FuncMul_Click);
+            // 
+            // FuncSub
+            // 
+            this.FuncSub.Location = new System.Drawing.Point(234, 124);
+            this.FuncSub.Name = "FuncSub";
+            this.FuncSub.Size = new System.Drawing.Size(40, 40);
+            this.FuncSub.TabIndex = 12;
+            this.FuncSub.Text = "-";
+            this.FuncSub.UseVisualStyleBackColor = true;
+            this.FuncSub.Click += new System.EventHandler(this.FuncSub_Click);
+            // 
+            // FuncAdd
+            // 
+            this.FuncAdd.Location = new System.Drawing.Point(234, 60);
+            this.FuncAdd.Name = "FuncAdd";
+            this.FuncAdd.Size = new System.Drawing.Size(40, 40);
+            this.FuncAdd.TabIndex = 11;
+            this.FuncAdd.Text = "+";
+            this.FuncAdd.UseVisualStyleBackColor = true;
+            this.FuncAdd.Click += new System.EventHandler(this.FuncAdd_Click);
+            // 
+            // FuncDiv
+            // 
+            this.FuncDiv.Location = new System.Drawing.Point(234, 253);
+            this.FuncDiv.Name = "FuncDiv";
+            this.FuncDiv.Size = new System.Drawing.Size(40, 40);
+            this.FuncDiv.TabIndex = 17;
+            this.FuncDiv.Text = "/";
+            this.FuncDiv.UseVisualStyleBackColor = true;
+            this.FuncDiv.Click += new System.EventHandler(this.FuncDiv_Click);
+            // 
+            // FuncCalc
+            // 
+            this.FuncCalc.Location = new System.Drawing.Point(160, 253);
+            this.FuncCalc.Name = "FuncCalc";
+            this.FuncCalc.Size = new System.Drawing.Size(40, 40);
+            this.FuncCalc.TabIndex = 16;
+            this.FuncCalc.Text = "=";
+            this.FuncCalc.UseVisualStyleBackColor = true;
+            this.FuncCalc.Click += new System.EventHandler(this.FuncCalc_Click);
+            // 
+            // Num0
+            // 
+            this.Num0.Location = new System.Drawing.Point(86, 253);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(40, 40);
+            this.Num0.TabIndex = 15;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            this.Num0.Click += new System.EventHandler(this.Num0_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 314);
+            this.Controls.Add(this.FuncDiv);
+            this.Controls.Add(this.FuncCalc);
+            this.Controls.Add(this.Num0);
+            this.Controls.Add(this.FuncMul);
+            this.Controls.Add(this.FuncSub);
+            this.Controls.Add(this.FuncAdd);
+            this.Controls.Add(this.Input_Text);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num7);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.TextBox Input_Text;
+        private System.Windows.Forms.Button FuncMul;
+        private System.Windows.Forms.Button FuncSub;
+        private System.Windows.Forms.Button FuncAdd;
+        private System.Windows.Forms.Button FuncDiv;
+        private System.Windows.Forms.Button FuncCalc;
+        private System.Windows.Forms.Button Num0;
+    }
+}
+
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Num1_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("1");
+        }
+
+        private void Num2_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("2");
+        }
+
+        private void Num3_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("3");
+        }
+
+        private void Num4_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("4");
+        }
+
+        private void Num5_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("5");
+        }
+
+        private void Num6_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("6");
+        }
+
+        private void Num7_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("7");
+        }
+
+        private void Num8_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("8");
+        }
+
+        private void Num9_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("9");
+        }
+
+        private void NumPeriod_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText(".");
+        }
+
+        private void Num0_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("0");
+        }
+
+        private void FuncCalc_Click(object sender, EventArgs e)
+        {
+            int calculated = 0;
+            int num1 = 0;
+            int num2 = 0;
+            Boolean num1Flag = false;
+            Boolean num2Flag = false;
+            String input = Input_Text.Text;
+            Char[] input_array = input.ToCharArray();
+            try
+            {
+
+                String function;
+                while (true)
+                {
+                    num1 = 0;
+                    num2 = 0;
+                    num1Flag = false;
+                    num2Flag = false;
+                    int indexCalc = input.IndexOf("/");
+                    if (indexCalc != -1)
+                            {
+                        function = "Divide";
+                    }
+                    else
+                    {
+                        indexCalc = input.IndexOf("*");
+                        if (indexCalc != -1)
+                        {
+                            function = "Multiply";
+                        }
+                        else
+                        {
+                            indexCalc = input.IndexOf("+");
+                            if (indexCalc != -1)
+                            {
+                                function = "Addition";
+                            }
+                            else
+                            {
+                                indexCalc = input.IndexOf("-");
+                                if (indexCalc != -1)
+                                {
+                                    function = "Subtraction";
+                                }
+                                else
+                                {
+                                    Input_Text.Text = input;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for (int i = 1; i <= indexCalc; i++)
+                    {
+                        if (char.IsDigit(input_array[indexCalc - i]))
+                        {
+                            if (!num1Flag)
+                            {
+                                num1 += Convert.ToInt32(Char.GetNumericValue(input_array[indexCalc - i]) * (Math.Pow(10,i - 1)));
+                            }
+                        }
+                        else
+                        {
+                            if (i == 1)
+                            {
+                                Input_Text.Text = "ERROR";
+                                break;
+                            }
+                            else
+                            {
+                                num1Flag = true;
+                                break;
+                            }
+                        }
+
+                    }
+                    for (int i = 1; i < input.Length-indexCalc; i++)
+                    {
+                        if (char.IsDigit(input_array[indexCalc + i]))
+                        {
+                            if (!num2Flag)
+                            {
+                                num2 += Convert.ToInt32(Char.GetNumericValue(input_array[indexCalc + i]) * (Math.Pow(10, i - 1)));
+                            }
+                        }
+                        else
+                        {
+                            if (i == 1)
+                            {
+                                Input_Text.Text = "ERROR";
+                                break;
+                            }
+                            else
+                            {
+                                num2Flag = true;
+                                break;
+                            }
+                        }
+
+                    }
+                    String replace_string="";
+                    switch (function)
+                    {
+                        case "Divide":
+                            calculated = num1 / num2;
+                            replace_string = num1.ToString() + "/" + num2.ToString();
+                            break;
+                        case "Multiply":
+                            calculated = num1 * num2;
+                            replace_string = num1.ToString() + "*" + num2.ToString();
+                            break;
+                        case "Addition":
+                            calculated = num1 + num2;
+                            replace_string = num1.ToString() + "+" + num2.ToString();
+                            break;
+                        case "Subtraction":
+                            calculated = num1 - num2;
+                            replace_string = num1.ToString() + "-" + num2.ToString();
+                            break;
+                    }
+                    Boolean check = input.Contains(replace_string);
+                    input = input.Replace(replace_string, calculated.ToString());
+                    input_array = input.ToCharArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Input_Text.Text = ("Error: " + ex.Message);
+            }
+
+    }
+
+    private void FuncAdd_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("+");
+        }
+
+        private void FuncSub_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("-");
+        }
+
+        private void FuncMul_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("*");
+        }
+
+        private void FuncDiv_Click(object sender, EventArgs e)
+        {
+            Input_Text.AppendText("/");
+        }
+    }
+}
